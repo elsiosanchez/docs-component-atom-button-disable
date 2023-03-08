490f0ec891b6e7339075ec444e200a7b6b743aed
Elsio Sanchez <45974454+elsiosanchez@users.noreply.github.com>
2023-03-08 16:34:38 -0400

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-card class="box-card">
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header-attributes">
+          <p class="title">
+            <b>
+              <i>
+                {{ "Ejemplo" }}
+              </i>
+            </b>
+          </p>
+        </div>
+      </template>
+      <div style="height: 50px; text-align: center">
+        <el-button :disabled="true">
+          {{ input }}
+        </el-button>
+        <el-button :disabled="false">
+          {{ input }}
+        </el-button>
+      </div>
+    </el-card>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const input = ref("Ejemplo");
+</script>
+
+<style scoped>
+.card-header-attributes {
+  text-align: center;
+}
+.title {
+  font-size: 20px;
+}
+</style>
